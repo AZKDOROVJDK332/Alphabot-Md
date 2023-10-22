37f068547136dd4a645d77e59b8c19407fa57da5
AZKDOROVJDK332 <143708922+AZKDOROVJDK332@users.noreply.github.com>
2023-10-22 23:36:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 const {
    indonesia,
-   english,
-   spanyol
+   Arabic,
+   Arabic
 } = require(`./language`)
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
-   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+   alfa: '', //apabila link api eror, segera laporkan ke owner
 }
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
@@ -24,25 +24,25 @@ global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
-//Available in indonesia , english & spanyol 
+//Available in indonesia , Arabic & Arabic 
 //You can request others language 
-global.language = english //change indonesia to english if you don't understand the language used by the bot
+global.language = Arabic //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
 // Other
 global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
-global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
-global.myweb2 = "https://s.id/BOTGITHUB" //bebas
-global.email = "mdsandaru2@gmail.com" //bebas
+global.ownername = "MAZEN" //nama kalian
+global.myweb = "" //bebas asal jan hapus
+global.youtube = "" //bebas asal jan hapus
+global.myweb2 = "" //bebas
+global.email = "" //bebas
 global.region = "Buddhist" //bebas
-global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.timezone = 'Asia/yemen' //  timezone wib
+global.premium = ['994408514178'] //premium user
+global.owner = ["", "967773849005"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "967773849005" // nomor wa kalian
+global.ownernomerr = "+967773849005" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
